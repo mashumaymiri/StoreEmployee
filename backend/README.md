5db6b84823cd3af95912f964b9fd7ae289dfcc60
mashumaymiri <mashumaymiri@hotmail.com>
2023-03-13 19:04:14 +0300

    Backend init

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+# StoreEmployee backend 
+Backend for the application.
+
+## Languages
+JAVA (Spring Boot framework)
+
+## Resources
+
+- Install and building (https://spring.io/quickstart)
---
+++ backend/StoreEmployee/src/test/java/com/example/StoreEmployee/StoreEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StoreEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
